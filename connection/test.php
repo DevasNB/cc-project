bddf85cd4a7371fd70fbed25cddb321754b644e1
andrei <andreinuca@gmail.com>
2024-06-10 13:36:39 +0100

    Add test connenction

---
+++ connection/test.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "10.0.1.4";
+$database = "mysql";
+$username = "CCPDBUser";
+$password = "CCPDBPassword";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+// Check connection
+if ($conn->connect_error) {
+die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
