184f8af5a85c65e910d4905efa0f92b49dfee198
andrei <andreinuca@gmail.com>
2024-06-10 15:48:43 +0100

    fix connection string

--- connection/connection.php
+++ connection/connection.php
@@ -6,10 +6,8 @@ $database = "carros";
 $username = "CCPDBUser";
 $password = "CCPDBPassword";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname, 3306);
 
 if ($conn->connect_error) {
-    die("Conexão falhou: " . $conn->connect_error);
+      die("Conexão falhou: " . $conn->connect_error);
 }
-
-?>
